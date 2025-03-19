dbbd58faeda0136a8288007519d5db06180f0d0b
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2025-03-19 12:55:29 +0200

    hand write server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,27 @@
 const path = require("path")
+const http = require("http")
 
 const express = require("express")
-const PORT = process.env.PORT || 3000
 
 const morgan = require("morgan")
 
+const WebSocket = require("ws")
+
 const app = express()
+const server = http.createServer(app)
 
 app.use(morgan("combined"))
+
+app.get("/", (req, res, next) => {
+    const wss = new WebSocket.Server({ server })
+
+    wss.on("connection", ws => {
+        ws.on("message", msg => {});
+        ws.on("close", () => {})
+    })
+
+    next()
+})
 
 app.use(express.static(
     path.join(__dirname, '../public')
@@ -17,6 +31,8 @@ app.get("/healthz", (_, res) => {
     res.send("OK")
 })
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
     console.log(`Server is runing on port ${PORT}`)
 })
+
